71db7b9d6f6cdb1ce2c6fc49ef9952164f674e71
Chiikritoo <anis.chikri27@gmail.com>
2026-08-17 23:51:59 +0200

    feat(render): draw a hardcoded triangle via VAO/VBO and the Shader class

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,48 @@
 /*   By: anchikri <anchikri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 19:26:20 by anchikri          #+#    #+#             */
-/*   Updated: 2026/08/17 19:26:22 by anchikri         ###   ########.fr       */
+/*   Updated: 2026/08/17 23:39:43 by anchikri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.hpp"
 #include "Window.hpp"
+#include "Shader.hpp"
 
 int	main(void)
 {
 	try {
 		Window	window(800, 600, "scop");
+		Shader	shader("shaders/basic.vert", "shaders/basic.frag");
+
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f,  0.5f, 0.0f
+		};
+
+		GLuint VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		GLuint VAO;
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
 
 		while (!window.shouldClose()) {
 			window.pollEvents();
 			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+			shader.use();
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 			window.swapBuffers();
 		}
 	} catch (const std::exception &e) {
