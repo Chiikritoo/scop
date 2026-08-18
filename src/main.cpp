c5b3b8c0d06b493481653c53765f5a294aad6f16
chiikritoo <anis.chikri27@gmail.com>
2026-08-18 17:22:49 +0200

    feat(render): pass a per-vertex color through the shader pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,44 @@
 /*   By: anchikri <anchikri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 19:26:20 by anchikri          #+#    #+#             */
-/*   Updated: 2026/08/17 23:39:43 by anchikri         ###   ########.fr       */
+/*   Updated: 2026/08/18 17:16:59 by anchikri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.hpp"
 #include "Window.hpp"
 #include "Shader.hpp"
+#include <cmath>
 
-int	main(void)
+int main(void)
 {
 	try {
-		Window	window(800, 600, "scop");
-		Shader	shader("shaders/basic.vert", "shaders/basic.frag");
+		Window  window(800, 600, "scop");
+		Shader  shader("shaders/basic.vert", "shaders/basic.frag");
+
+		// float vertices[] = {
+		//  -0.5f, -0.5f, 0.0f,
+		//  0.5f, -0.5f, 0.0f,
+		//  0.0f,  0.5f, 0.0f
+		// };
 
 		float vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f,  0.5f, 0.0f
-		};
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+				};
+
+		// float vertices[] = {
+		//  0.5f,  0.5f, 0.0f,  // top right
+		//  0.5f, -0.5f, 0.0f,  // bottom right
+		//  -0.5f, -0.5f, 0.0f,  // bottom left
+		//  -0.5f,  0.5f, 0.0f   // top left
+		// };
+		// unsigned int indices[] = {  // note that we start from 0!
+		//  0, 1, 3,   // first triangle
+		//  1, 2, 3    // second triangle
+		// };
 
 		GLuint VBO;
 		glGenBuffers(1, &VBO);
@@ -35,16 +54,32 @@ int	main(void)
 		glGenVertexArrays(1, &VAO);
 		glBindVertexArray(VAO);
 
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		// glEnableVertexAttribArray(0);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		// position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
+				// color attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		// unsigned int EBO;
+		// glGenBuffers(1, &EBO);
+		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		while (!window.shouldClose()) {
 			window.pollEvents();
 			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+			// float timeValue = glfwGetTime();
+			// float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+			// int vertexColorLocation = glGetUniformLocation(shader.getId(), "ourColor");
+			// glUseProgram(shader.getId());
+			// glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 			shader.use();
 			glBindVertexArray(VAO);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
